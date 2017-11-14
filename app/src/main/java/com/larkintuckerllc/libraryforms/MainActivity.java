987837a9f4f566dc277dbcb89858265e6ec9d878
John Tucker <john@larkintuckerllc.com>
2017-11-14 18:17:59 -0500

    Maybe final

--- app/src/main/java/com/larkintuckerllc/libraryforms/MainActivity.java
+++ app/src/main/java/com/larkintuckerllc/libraryforms/MainActivity.java
@@ -6,6 +6,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
@@ -21,11 +22,14 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
 
     @NotEmpty(message = "Required")
     private EditText mLastField;
-
     @Email(message = "Valid Email Required")
     private EditText mEmailField;
 
-    private Validator validator;
+    private Button mSubmitButton;
+    private Validator mValidator;
+    private Boolean mFirstFieldDirty = false;
+    private Boolean mLastFieldDirty = false;
+    private Boolean mEmailFieldDirty = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +37,17 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
         setContentView(R.layout.activity_main);
         mFirstField = findViewById(R.id.activity_main__txt_first);
         mFirstField.addTextChangedListener(new FieldValidator());
+        mFirstField.setOnFocusChangeListener(new FieldFocusChangeListener());
         mLastField = findViewById(R.id.activity_main__txt_last);
         mLastField.addTextChangedListener(new FieldValidator());
+        mLastField.setOnFocusChangeListener(new FieldFocusChangeListener());
         mEmailField = findViewById(R.id.activity_main__txt_email);
         mEmailField.addTextChangedListener(new FieldValidator());
-        validator = new Validator(this);
-        validator.setValidationListener(this);
+        mEmailField.setOnFocusChangeListener(new FieldFocusChangeListener());
+        mSubmitButton = findViewById(R.id.activity_main__btn_submit);
+        mSubmitButton.setEnabled(false);
+        mValidator = new Validator(this);
+        mValidator.setValidationListener(this);
     }
 
     public void sendFeedback(View view) {
@@ -52,15 +61,30 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
 
     @Override
     public void onValidationSucceeded() {
+        mSubmitButton.setEnabled(true);
     }
 
     @Override
     public void onValidationFailed(List<ValidationError> errors) {
+        mSubmitButton.setEnabled(false);
         for (ValidationError error : errors) {
             View view = error.getView();
             String message = error.getCollatedErrorMessage(this);
             if (view instanceof EditText) {
-                ((EditText) view).setError(message);
+                switch(view.getId()) {
+                    case R.id.activity_main__txt_first:
+                        if (mFirstFieldDirty)
+                            ((EditText) view).setError(message);
+                            break;
+                    case R.id.activity_main__txt_last:
+                        if (mLastFieldDirty)
+                            ((EditText) view).setError(message);
+                            break;
+                    case R.id.activity_main__txt_email:
+                        if (mEmailFieldDirty)
+                            ((EditText) view).setError(message);
+                            break;
+                }
             }
         }
     }
@@ -71,10 +95,29 @@ public class MainActivity extends AppCompatActivity implements Validator.Validat
         }
 
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            validator.validate();
+            mValidator.validate();
         }
 
         public void afterTextChanged(Editable s) {
+        }
+    }
+
+    private class FieldFocusChangeListener implements View.OnFocusChangeListener {
+        @Override
+        public void onFocusChange(View view, boolean hasFocus) {
+            if (hasFocus) return;
+            switch(view.getId()) {
+                case R.id.activity_main__txt_first:
+                    mFirstFieldDirty = true;
+                    break;
+                case R.id.activity_main__txt_last:
+                    mLastFieldDirty = true;
+                    break;
+                case R.id.activity_main__txt_email:
+                    mEmailFieldDirty = true;
+                    break;
+            }
+            mValidator.validate();
         }
     }
 
